3a83e2aa4fe4151980889ec579d819ca2aa2bd7d
Katie Konrad <kathrynkonrad@gmail.com>
2020-05-10 15:00:51 -0400

    Home Screen

---
+++ components/HomeComponent.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Animated } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           squareColorValue: new Animated.Value(0)
+        };
+    }
+
+    animateViewColor() {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(this.state.squareColorValue, {
+                toValue: 1,
+                duration: 5000
+                })
+            ])
+        ).start();
+    }
+
+    componentDidMount() {
+        this.animateViewColor();
+    }
+
+    render() {
+        const bgColor = this.state.squareColorValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['blue', 'yellow', 'red']
+        });
+          
+        return (
+            <View style={styles.container}>
+                <Text>{`Welcome to Morectangles!`}</Text>
+                <Text>{`Would you like to play?\n`}</Text>
+                <Animatable.View style={{padding: 50, backgroundColor: bgColor}} onPress={() => <GameComponent />} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    SquareView: {
+        width: 120,
+        height: 120,
+    }
+})
+
+export default Home;
---
+++ components/MainComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+          
+        return (
+            <Home />
+        )
+    }
+}
+
+
+export default Main;
